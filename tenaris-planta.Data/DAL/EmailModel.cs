e5f5d3dd032beca96a3d516354987db3551ee86b
gchervet <gonzalochervet@outlook.com>
2020-03-12 13:17:25 -0300

    U/ - Added app variables on web config

--- tenaris-planta.Data/DAL/EmailModel.cs
+++ tenaris-planta.Data/DAL/EmailModel.cs
@@ -4,6 +4,7 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -17,50 +18,11 @@ namespace tenaris_planta.Data.DAL
 {
     public class EmailModel
     {
-        /*
-        public EmailModel()
-        {
-            // See https://www.elastic.co/guide/en/elasticsearch/client/net-api/current/nest-getting-started.html
-            ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, errors) => true;
-
-            var uris = new[]
-            {
-                new Uri("https://54.162.155.36:9200/"),
-            };
-            // new Uri("https://54.162.155.36:9200/emails"),
-
-            var connectionPool = new SniffingConnectionPool(uris);
-            var connectionConfiguration = new ConnectionConfiguration()
-                .DisableAutomaticProxyDetection()
-                .EnableHttpCompression()
-                .DisableDirectStreaming()
-                .PrettyJson()
-                .RequestTimeout(TimeSpan.FromMinutes(2));
-
-            var lowLevelClient = new ElasticLowLevelClient(connectionConfiguration);
-
-            // var searchResponseLow = lowLevelClient.Search<JObject>("emails", null);
-
-            var settings = new ConnectionSettings(connectionPool)
-                .ServerCertificateValidationCallback(CertificateValidations.AllowAll)
-                .DefaultIndex("emails")
-                .BasicAuthentication("admin", "admin");
-            
-            var client = new ElasticClient(settings);
-
-            var searchResponse = client.Search<object>(s => s
-                                    .AllIndices()
-                                    .Query(q => q));
-
-            var emails = searchResponse.Documents;
-        }
-        */
-
         public EmailModel() { }
         public static object Get(string id)
         {
             string rtn = string.Empty;
-            string url = @"https://54.162.155.36:9200/emails/_search";
+            string url = ConfigurationManager.AppSettings["ElasticsearchServer"] + @"/emails/_search";
             if (!string.IsNullOrEmpty(id))
             {
                 url += "?q=_id: " + id;
@@ -72,8 +34,9 @@ namespace tenaris_planta.Data.DAL
                delegate { return true; }
             );
 
+            // Request data
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-            request.Headers.Add("Authorization", "Basic YWRtaW46YWRtaW4=");
+            request.Headers.Add("Authorization", ConfigurationManager.AppSettings["ElasticsearchBasicAuthBase64"]);
 
             using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
             using (Stream stream = response.GetResponseStream())
@@ -88,7 +51,7 @@ namespace tenaris_planta.Data.DAL
         public static object Update(string id, JObject updateObject)
         {
             string html = string.Empty;
-            string url = @"https://54.162.155.36:9200/emails/_update/" + id;
+            string url = ConfigurationManager.AppSettings["ElasticsearchServer"] + @"/emails/_update/" + id;
 
             ServicePointManager.ServerCertificateValidationCallback = new
             RemoteCertificateValidationCallback
@@ -96,9 +59,10 @@ namespace tenaris_planta.Data.DAL
                delegate { return true; }
             );
 
+            // Request data
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
             request.Method = "POST";
-            request.Headers.Add("Authorization", "Basic YWRtaW46YWRtaW4=");
+            request.Headers.Add("Authorization", ConfigurationManager.AppSettings["ElasticsearchBasicAuthBase64"]);
 
             // POST data
             byte[] data = Encoding.ASCII.GetBytes(JsonConvert.SerializeObject(updateObject));
@@ -110,13 +74,10 @@ namespace tenaris_planta.Data.DAL
             requestStream.Close();
 
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-
             Stream responseStream = response.GetResponseStream();
-
             StreamReader streamReader = new StreamReader(responseStream, Encoding.Default);
 
             string rtn = streamReader.ReadToEnd();
-
             streamReader.Close();
             responseStream.Close();
 
